fad5a5adefe1092c58a8892407051e39be5d349e
lakathoa <86317065+lakathoa@users.noreply.github.com>
2025-02-12 15:12:33 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false, //para que se vea o no el browser cuando ejecuta los test false muestra el br y true no lo muestra
+    headless: true, //para que se vea o no el browser cuando ejecuta los test false muestra el br y true no lo muestra
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
