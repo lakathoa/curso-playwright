e39e5ebddc72752157beb1e2ed7e78f41a3c5fff
lakathoa <86317065+lakathoa@users.noreply.github.com>
2025-02-12 14:58:12 -0500

    Update NavigateTo.ts

--- page-objects/navigate/NavigateTo.ts
+++ page-objects/navigate/NavigateTo.ts
@@ -8,6 +8,6 @@ export class NavigateTo {
     }
 
     async loginPage(){
-        await this.page.goto('http://127.0.0.1:5500/login.html')
+        await this.page.goto('http://127.0.0.1:5500/login/login.html')
     }
 }
